6f85d42a1e9c043217b5a56f09327ee7bfa88dac
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-06-01 00:55:38 -0300

    praticing fetch and async await

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 window.addEventListener( "load", function() {
 
-    fetch("http://api.github.com/users/zmmarina")
-    .then( res => {res.json()
-        .then( data => { showData(data);
-    });
+    doFetch();
+    doFetchAsync();
+    
+    executeDivisionPromise();
+    executeDivisionPromiseAsync();
 });
 
-});
+function doFetch(){
+    fetch("http://api.github.com/users/zmmarina")
+        .then( res => {
+            res.json().then( data => { 
+                showData(data);
+            });
+        })      
+        .catch( error => {
+            console.log("Request error");
+        });
+}
+
+async function doFetchAsync(){
+    const res = await fetch("http://api.github.com/users/zmmarina");
+    const json = await res.json();
+    console.log(json);
+}
+
+
 
 function showData (data) {
 
@@ -14,3 +33,32 @@ function showData (data) {
     user.textContent = data.login + " " + data.name;
 }
 
+function divisionPromise( a, b){
+    return new Promise( (resolve, reject) => { 
+        if (b === 0){
+            reject ("It is not possible division by zero");
+        }
+
+        resolve (a / b);
+    });
+}
+
+function executeDivisionPromise(){
+
+    divisionPromise( 12, 6).then( result => {
+        console.log( result);
+    });
+
+    divisionPromise( 12, 0).then( result => {
+        console.log( result);
+    })
+    .catch( errorMessage => {
+        console.log("Division failure" + errorMessage);
+    });
+}
+
+async function executeDivisionPromiseAsync(){
+
+    const division = await divisionPromise(12, 2);
+    console.log(division);    
+}
